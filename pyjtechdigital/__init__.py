780f0b7d7c88a44179d411747d76d10b0dc345e5
AlexOwl <AlexOwl@protonmail.com>
2023-07-28 04:35:39 +0400

    v0.3.1

--- pyjtechdigital/__init__.py
+++ pyjtechdigital/__init__.py
@@ -4,4 +4,4 @@ from .client import JtechClient
 from .responses import JtechResponse, JtechLoginResponse, JtechNetworkResponse, JtechStatusResponse, JtechVideoStatusResponse, JtechOutputStatusResponse, JtechInputStatusResponse, JtechCECStatusResponse, JtechSystemStatusResponse
 from .exceptions import JtechError, JtechAuthError, JtechNotSupported, JtechConnectionError, JtechConnectionTimeout, JtechInvalidSource, JtechInvalidOutput, JtechOptionError
 
-__version__ = "0.2.2"
+__version__ = "0.3.1"
